6f37700f439a1327166587a7f454b514799bd6bf
Uttam sharma <uttamftspl@gmail.com>
2024-12-16 21:53:18 +0530

    youtube_clone app

---
+++ youtube/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './Components/navbar/Navbar'
+
+const App = () => {
+  return (
+    <div>
+      
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
