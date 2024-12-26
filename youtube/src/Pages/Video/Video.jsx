c1a911403b4069e31f215167d57b18b537504742
Uttam sharma <uttamftspl@gmail.com>
2024-12-26 23:24:46 +0530

    video play page

--- youtube/src/Pages/Video/Video.jsx
+++ youtube/src/Pages/Video/Video.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import './video.css'
+import PlayVideo from '../../Components/PlayVideo/PlayVideo'
 const Video = () => {
   return (
-    <div>Video</div>
+    <div className='play-container'>
+      <PlayVideo/>
+    </div>
   )
 }
 
