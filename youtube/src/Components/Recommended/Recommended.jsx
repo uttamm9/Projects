364d1bde88ebe6dabd658843daa27db45abc95ec
Uttam sharma <uttamftspl@gmail.com>
2025-01-02 22:37:47 +0530

    recommended component

---
+++ youtube/src/Components/Recommended/Recommended.jsx
@@ -0,0 +1,83 @@
+import React from 'react'
+import './Recommended.css'
+import thumbnain1 from '../../assets/thumbnail1.png'
+import thumbnain2 from '../../assets/thumbnail2.png'
+import thumbnain3 from '../../assets/thumbnail3.png'
+import thumbnain4 from '../../assets/thumbnail4.png'
+import thumbnain5 from '../../assets/thumbnail5.png'
+import thumbnain6 from '../../assets/thumbnail6.png'
+import thumbnain7 from '../../assets/thumbnail7.png'
+import thumbnain8 from '../../assets/thumbnail8.png'
+
+const Recommended = () => {
+  return (
+    <div className='recommended'>
+      <div className="side-video-list">
+        <img src={thumbnain1} alt="" />
+        <div className='vid-info'>
+          <h4>Best channel that help you to be a web developer</h4>
+          <p>the uttam</p>
+          <p>211K Views</p>
+        </div>
+      </div>
+      <div className="side-video-list">
+        <img src={thumbnain2} alt="" />
+        <div className='vid-info'>
+          <h4>Best channel that help you to be a web developer</h4>
+          <p>the uttam</p>
+          <p>211K Views</p>
+        </div>
+      </div>
+      <div className="side-video-list">
+        <img src={thumbnain3} alt="" />
+        <div className='vid-info'>
+          <h4>Best channel that help you to be a web developer</h4>
+          <p>the uttam</p>
+          <p>211K Views</p>
+        </div>
+      </div>
+      <div className="side-video-list">
+        <img src={thumbnain4} alt="" />
+        <div className='vid-info'>
+          <h4>Best channel that help you to be a web developer</h4>
+          <p>the uttam</p>
+          <p>211K Views</p>
+        </div>
+      </div>
+      <div className="side-video-list">
+        <img src={thumbnain5} alt="" />
+        <div className='vid-info'>
+          <h4>Best channel that help you to be a web developer</h4>
+          <p>the uttam</p>
+          <p>211K Views</p>
+        </div>
+      </div>
+      <div className="side-video-list">
+        <img src={thumbnain6} alt="" />
+        <div className='vid-info'>
+          <h4>Best channel that help you to be a web developer</h4>
+          <p>the uttam</p>
+          <p>211K Views</p>
+        </div>
+      </div>
+      <div className="side-video-list">
+        <img src={thumbnain7} alt="" />
+        <div className='vid-info'>
+          <h4>Best channel that help you to be a web developer</h4>
+          <p>the uttam</p>
+          <p>211K Views</p>
+        </div>
+      </div>
+      <div className="side-video-list">
+        <img src={thumbnain8} alt="" />
+        <div className='vid-info'>
+          <h4>Best channel that help you to be a web developer</h4>
+          <p>the uttam</p>
+          <p>211K Views</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Recommended
