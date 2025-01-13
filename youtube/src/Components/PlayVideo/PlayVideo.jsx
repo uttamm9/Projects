7b4be860ba9da86dddfc2bfc7e69b5857b1eb3cb
Uttam sharma <uttamftspl@gmail.com>
2025-01-13 23:57:15 +0530

    update in youtube

--- youtube/src/Components/PlayVideo/PlayVideo.jsx
+++ youtube/src/Components/PlayVideo/PlayVideo.jsx
@@ -12,6 +12,7 @@ import moment from 'moment'
 const PlayVideo = ({videoId}) => {
   const [apidata, setApidata] = React.useState(null)
   const [channelData, setChannelData] = React.useState(null)
+  const [commentData, setCommentData] = React.useState(null)
 
   const fatchVideoData = async ()=>{
     //Fetch video data from youtube api
@@ -24,6 +25,9 @@ const PlayVideo = ({videoId}) => {
     //Fetch channel data from youtube api
     const channelData_url = `https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=${apidata.snippet.channelId}&key=${API_KEY}`;
     await fetch(channelData_url).then(res=>res.json()).then(data=>setChannelData(data.items[0]))
+    //fatch comment data from youtube api
+    const comment_url = `https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&videoId=${videoId}&key=${API_KEY}`
+    await fetch(comment_url).then(res=>res.json()).then(data=>setCommentData(data.items))
   }
 
   useEffect(()=>{
@@ -49,10 +53,10 @@ const PlayVideo = ({videoId}) => {
       </div>
       <hr />
       <div className="publisher">
-        <img src={jack} alt="" />
+        <img src={channelData?channelData.snippet.thumbnails.default.url:""} alt="" />
         <div>
           <p>{apidata?apidata.snippet.channelTitle:""}</p>
-          <span>{apidata?apidata.snippet:""} Subcribers</span>
+          <span>{channelData?valueConverter(channelData.statistics.subscriberCount):"0"} Subcribers</span>
         </div>
         <button>Subcribe</button>
       </div>
@@ -60,66 +64,24 @@ const PlayVideo = ({videoId}) => {
         <p>{apidata?apidata.snippet.description.slice(0,250):""}</p>
         <hr />
         <h4>{apidata?valueConverter(apidata.statistics.commentCount):"0"} comments</h4>
-        <div className="comment">
-          <img src={user_profile} alt="" />
-          <div>
-            <h3>yogesh <span>1 day ago</span></h3>
-            <p>A globale network ptovideimng a variety of innformation and communication of interconnected network using standard communicartion protocall </p>
-            <div className="comment-action">
-              <img src={like} alt="" />
-              <span>244</span>
-              <img src={dislike} alt="" />
+        {commentData && commentData.map((item,index)=>{
+          return(
+            <div className="comment" key={index}>
+              <img src={item.snippet.topLevelComment.snippet.authorProfileImageUrl} alt="" />
+              <div>
+                <h3>{item.snippet.topLevelComment.snippet.authorDisplayName} <span>{moment(item.snippet.topLevelComment.snippet.publishedAt).fromNow()}</span></h3>
+                <p>{item.snippet.topLevelComment.snippet.textDisplay}</p>
+                <div className="comment-action">
+                  <img src={like} alt="" />
+                  <span>{item.snippet.topLevelComment.snippet.likeCount}</span>
+                  <img src={dislike} alt="" />
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-        <div className="comment">
-          <img src={user_profile} alt="" />
-          <div>
-            <h3>yogesh <span>1 day ago</span></h3>
-            <p>A globale network ptovideimng a variety of innformation and communication of interconnected network using standard communicartion protocall </p>
-            <div className="comment-action">
-              <img src={like} alt="" />
-              <span>244</span>
-              <img src={dislike} alt="" />
-            </div>
-          </div>
-        </div>
-        <div className="comment">
-          <img src={user_profile} alt="" />
-          <div>
-            <h3>yogesh <span>1 day ago</span></h3>
-            <p>A globale network ptovideimng a variety of innformation and communication of interconnected network using standard communicartion protocall </p>
-            <div className="comment-action">
-              <img src={like} alt="" />
-              <span>244</span>
-              <img src={dislike} alt="" />
-            </div>
-          </div>
-        </div>
-        <div className="comment">
-          <img src={user_profile} alt="" />
-          <div>
-            <h3>yogesh <span>1 day ago</span></h3>
-            <p>A globale network ptovideimng a variety of innformation and communication of interconnected network using standard communicartion protocall </p>
-            <div className="comment-action">
-              <img src={like} alt="" />
-              <span>244</span>
-              <img src={dislike} alt="" />
-            </div>
-          </div>
-        </div>
-        <div className="comment">
-          <img src={user_profile} alt="" />
-          <div>
-            <h3>yogesh <span>1 day ago</span></h3>
-            <p>A globale network ptovideimng a variety of innformation and communication of interconnected network using standard communicartion protocall </p>
-            <div className="comment-action">
-              <img src={like} alt="" />
-              <span>244</span>
-              <img src={dislike} alt="" />
-            </div>
-          </div>
-        </div>
+          )
+        })}
+       
+        
       </div>
     </div>
   )
--- youtube/src/Components/Recommended/Recommended.jsx
+++ youtube/src/Components/Recommended/Recommended.jsx
@@ -1,81 +1,38 @@
 import React from 'react'
 import './Recommended.css'
-import thumbnain1 from '../../assets/thumbnail1.png'
-import thumbnain2 from '../../assets/thumbnail2.png'
-import thumbnain3 from '../../assets/thumbnail3.png'
-import thumbnain4 from '../../assets/thumbnail4.png'
-import thumbnain5 from '../../assets/thumbnail5.png'
-import thumbnain6 from '../../assets/thumbnail6.png'
-import thumbnain7 from '../../assets/thumbnail7.png'
-import thumbnain8 from '../../assets/thumbnail8.png'
 
-const Recommended = () => {
+import { API_KEY } from '../../data'
+import { valueConverter } from '../../data'
+const Recommended = ({categoryId}) => {
+    const [apidata, setApidata] = React.useState([]);
+
+    const fetchVideoData = async ()=>{
+      //Fetch video data from youtube api
+      const RelatedVideo_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&videoCategoryId=${categoryId}&key=${API_KEY}`
+
+      await fetch(RelatedVideo_url).then(res=>res.json()).then(data=>setApidata(data.items))
+
+    }
+    React.useEffect(()=>{
+      fetchVideoData();
+    },[categoryId])
+
   return (
     <div className='recommended'>
-      <div className="side-video-list">
-        <img src={thumbnain1} alt="" />
-        <div className='vid-info'>
-          <h4>Best channel that help you to be a web developer</h4>
-          <p>the uttam</p>
-          <p>211K Views</p>
-        </div>
-      </div>
-      <div className="side-video-list">
-        <img src={thumbnain2} alt="" />
-        <div className='vid-info'>
-          <h4>Best channel that help you to be a web developer</h4>
-          <p>the uttam</p>
-          <p>211K Views</p>
-        </div>
-      </div>
-      <div className="side-video-list">
-        <img src={thumbnain3} alt="" />
-        <div className='vid-info'>
-          <h4>Best channel that help you to be a web developer</h4>
-          <p>the uttam</p>
-          <p>211K Views</p>
-        </div>
-      </div>
-      <div className="side-video-list">
-        <img src={thumbnain4} alt="" />
-        <div className='vid-info'>
-          <h4>Best channel that help you to be a web developer</h4>
-          <p>the uttam</p>
-          <p>211K Views</p>
-        </div>
-      </div>
-      <div className="side-video-list">
-        <img src={thumbnain5} alt="" />
-        <div className='vid-info'>
-          <h4>Best channel that help you to be a web developer</h4>
-          <p>the uttam</p>
-          <p>211K Views</p>
-        </div>
-      </div>
-      <div className="side-video-list">
-        <img src={thumbnain6} alt="" />
-        <div className='vid-info'>
-          <h4>Best channel that help you to be a web developer</h4>
-          <p>the uttam</p>
-          <p>211K Views</p>
-        </div>
-      </div>
-      <div className="side-video-list">
-        <img src={thumbnain7} alt="" />
-        <div className='vid-info'>
-          <h4>Best channel that help you to be a web developer</h4>
-          <p>the uttam</p>
-          <p>211K Views</p>
-        </div>
-      </div>
-      <div className="side-video-list">
-        <img src={thumbnain8} alt="" />
-        <div className='vid-info'>
-          <h4>Best channel that help you to be a web developer</h4>
-          <p>the uttam</p>
-          <p>211K Views</p>
-        </div>
-      </div>
+      {apidata.map((item,index)=>{
+        return (
+          <div key={index} className='video'>
+            <img src={item.snippet.thumbnails.medium.url} alt="" />
+            <div className='video-info'>
+              <h4>{item.snippet.title}</h4>
+              <p>{item.snippet.channelTitle}</p>
+              <p>{valueConverter(item.statistics.viewCount)} views &bull; 1 week ago</p>
+            </div>
+          </div>
+        )
+      })}
+      
+      
     </div>
   )
 }
