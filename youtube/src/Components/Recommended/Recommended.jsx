1730e6565103694806b82ae4c41867906f8ea6d9
Uttam sharma <uttamftspl@gmail.com>
2025-01-14 21:53:23 +0530

    update recommended list data

--- youtube/src/Components/Recommended/Recommended.jsx
+++ youtube/src/Components/Recommended/Recommended.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Recommended.css'
-
+import { Link } from 'react-router-dom'
 import { API_KEY } from '../../data'
 import { valueConverter } from '../../data'
 const Recommended = ({categoryId}) => {
@@ -8,7 +8,7 @@ const Recommended = ({categoryId}) => {
 
     const fetchVideoData = async ()=>{
       //Fetch video data from youtube api
-      const RelatedVideo_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&videoCategoryId=${categoryId}&key=${API_KEY}`
+      const RelatedVideo_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=20&regionCode=US&videoCategoryId=${categoryId}&key=${API_KEY}`
 
       await fetch(RelatedVideo_url).then(res=>res.json()).then(data=>setApidata(data.items))
 
@@ -21,14 +21,14 @@ const Recommended = ({categoryId}) => {
     <div className='recommended'>
       {apidata.map((item,index)=>{
         return (
-          <div key={index} className='video'>
+          <Link to={`/video/${item.snippet.categoryId}/${item.id}`} key={index} className='side-video-list'>
             <img src={item.snippet.thumbnails.medium.url} alt="" />
             <div className='video-info'>
               <h4>{item.snippet.title}</h4>
               <p>{item.snippet.channelTitle}</p>
-              <p>{valueConverter(item.statistics.viewCount)} views &bull; 1 week ago</p>
+              <p>{valueConverter(item.statistics.viewCount)} views</p>
             </div>
-          </div>
+          </Link>
         )
       })}
       
